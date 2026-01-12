1502b9a13c901252e80ce34aec39c621d1a510c7
OisinSD <oisin.s.dillon@gmail.com>
2026-01-12 15:41:34 +0000

    task class, create file and print task methods

---
+++ tasks.java
@@ -0,0 +1,45 @@
+package taskmanagerEncryption;
+
+public class tasks {
+
+    private int taskIndex;
+	private String taskName = "";
+	private String taskDes = "";
+	private boolean taskStatus;
+	
+	
+	tasks(int taskIndex, String taskName, String taskDes, boolean taskStatus){
+		this.taskIndex = taskIndex;
+		this.taskName = taskName;
+		this.taskDes = taskDes;
+		this.taskStatus = taskStatus;
+	}
+	
+	public int getTaskIndex() {
+		return taskIndex; 
+	}
+	
+	public void setTaskIndex(int taskIndex) {
+		this.taskIndex = taskIndex;
+	}
+	
+	public String getTaskName() {
+		return taskName;
+	}
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	public String getTaskDes() {
+		return taskDes;
+	}
+	public void setTaskDes(String taskDes) {
+		this.taskDes = taskDes;
+	}
+	public boolean getTaskStatus(boolean taskStatus) {
+		return taskStatus;
+	}
+	public void setTaskStatus() {
+		this.taskStatus = taskStatus;
+	}
+}
+
